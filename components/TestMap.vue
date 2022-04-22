075a19083ebb1d6b1f844a8b9c97faabd0039f6e
Arnaud BREGERE <arnaud.bregere@gmail.com>
2022-04-22 15:58:29 +0200

    Add files via upload

---
+++ components/TestMap.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="map-wrap" style="height: 100vh; width: 100vh">
+    <client-only>
+      <l-map :zoom=13 :center="[48.864716,2.349014]">
+        <l-tile-layer url="http://{s}.tile.localhost:8080/tile/{z}/{x}/{y}.png"></l-tile-layer>
+        <l-marker :lat-lng="[48.864716,2.349014]"></l-marker>
+      </l-map>
+    </client-only>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TestMap"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+    Hello World Leaflet
+    </v-col>
+    <TestMap/>
+  </v-row>
+</template>
+
+<script>
+import TestMap from "~/components/TestMap";
+export default {
+  name: 'IndexPage',
+  components: {
+    TestMap
+  }
+}
+</script>
